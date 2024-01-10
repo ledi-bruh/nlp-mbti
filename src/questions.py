cf25bcf1077b1a1540409bed3bad29b7bdd46cde
Andrey Podovinov <andrey.podovinov@iqtek.ru>
2024-01-10 20:28:11 +0600

    update questions

--- src/questions.py
+++ src/questions.py
@@ -1,14 +1,42 @@
 questions = [
     {
-        "ru": "Как вы проведёте вечер после дня неудач и разочарований?",
+        "ru": "Как Вы проведёте вечер после дня неудач и разочарований?",
         "en": "How will you spend the evening after a day of failures and disappointments?",
     },
     {
-        "ru": "Как вы подготовитесь к долгожданному отпуску, который будет через месяц?",
+        "ru": "Как Вы подготовитесь к долгожданному отпуску, который будет через месяц?",
         "en": "How will you prepare for the long-awaited vacation that will be in a month?",
     },
     {
         "ru": "Вам предложили переехать в крупный город, где возможностей больше, чем там, где вы живете сейчас и где ваши близкие и дом. Какое решение вы примите?",
         "en": "You were offered to move to a large city where there are more opportunities than where you live now and where your loved ones and home are. What decision will you make?",
+    },
+    {
+        "ru": "Как Вы себя поведете, оказавшись в новом коллективе?",
+        "en": "How will you behave when you find yourself in a new team?",
+    },
+    {
+        "ru": "Что бы Вы предпочли, распланировать день или действовать спонтанно?",
+        "en": "Would you rather plan your day or act spontaneously?",
+    },
+    {
+        "ru": "Что для Вас самое важное в процессе работы?",
+        "en": "What is the most important thing for you in the process of work?",
+    },
+    {
+        "ru": "На что Вы полагаетесь при оценке людей?",
+        "en": "What do you rely on when evaluating people?",
+    },
+    {
+        "ru": "Что вы делаете, когда ситуация неожиданно меняется?",
+        "en": "What do you do when the situation suddenly changes?",
+    },
+    {
+        "ru": "В диалоге Вы больше слушаете или говорите?",
+        "en": "Do you listen or talk more in the dialogue?",
+    },
+    {
+        "ru": "Сколько у Вас друзей и знакомых?",
+        "en": "How many friends and acquaintances do you have?",
     },
 ]
